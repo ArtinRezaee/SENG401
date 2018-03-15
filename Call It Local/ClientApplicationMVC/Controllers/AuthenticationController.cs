5989d172b06143fe3a32cc3c0c79b2c3b104286b
Allan Varkey <amvarkey@gmail.com>
2018-03-14 18:49:09 -0600

    all the changes from allan

--- Call It Local/ClientApplicationMVC/Controllers/AuthenticationController.cs
+++ Call It Local/ClientApplicationMVC/Controllers/AuthenticationController.cs
@@ -6,24 +6,37 @@ using Messages.ServiceBusRequest;
 using Messages.ServiceBusRequest.Authentication.Requests;
 
 using System.Web.Mvc;
-using System;
 
 namespace ClientApplicationMVC.Controllers
 {
-	/// <summary>
-	/// This class contains the functions responsible for handling requests routed to *Hostname*/Authentication/*
-	/// </summary>
-	public class AuthenticationController : Controller
-	{
-		/// <summary>
-		/// The default method for this controller
-		/// </summary>
-		/// <returns>The login page</returns>
-		public ActionResult Index()
-		{
-			ViewBag.Message = "Please enter your username and password.";
-			return View("Index");
-		}
+    /// <summary>
+    /// This class contains the functions responsible for handling requests routed to *Hostname*/Authentication/*
+    /// </summary>
+    public class AuthenticationController : Controller
+    {
+        /// <summary>
+        /// The default method for this controller
+        /// </summary>
+        /// <returns>The login page</returns>
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Please enter your username and password.";
+            return View("Index");
+        }
+
+        //This class is incomplete and should be completed by the students in milestone 2
+        //Hint: You will need to make use of the ServiceBusConnection class. See EchoController.cs for an example.
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            ServiceBusConnection serviceBusConnection = new ServiceBusConnection(username);
+            LogInRequest logInRequest = new LogInRequest(username, password);
+             ServiceBusResponse result =serviceBusConnection.sendLogIn(logInRequest);
+            ViewBag.LoginResponse = result.response + " " +result.result;
+
+            return View("Index");
+
+        }
 
         public ActionResult CreateAccount()
         {
@@ -32,10 +45,10 @@ namespace ClientApplicationMVC.Controllers
         }
 
 
-		public ActionResult SignUp(string username, string email, string address, string phone, string password, string type)
-		{
+        public ActionResult SignUp(string username, string email, string address, string phone, string password, string type)
+        {
             AccountType accType;
-            switch(type)
+            switch (type)
             {
                 case "user":
                     accType = AccountType.user;
@@ -46,9 +59,10 @@ namespace ClientApplicationMVC.Controllers
                 default:
                     accType = AccountType.notspecified;
                     break;
-            }   
+            }
 
-			CreateAccount account = new CreateAccount() {
+            CreateAccount account = new CreateAccount()
+            {
                 username = username,
                 email = email,
                 address = address,
@@ -72,14 +86,15 @@ namespace ClientApplicationMVC.Controllers
                     return View("CreateAccount");
                 }
             }
-            catch(Exception err)
+            catch (Exception err)
             {
                 System.Diagnostics.Debug.WriteLine(err.Message);
                 ViewBag.Message = "Please fill out this form to sign up";
                 ViewBag.CreateAccountResponse = err.Message;
                 return View("CreateAccount");
             }
-		}
+        }
 
-	}
+
+    }
 }
